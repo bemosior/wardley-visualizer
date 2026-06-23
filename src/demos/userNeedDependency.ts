eed902f7fd9a86d76ead9cacc63e6404d1889510
Ben Mosior <bemosior@gmail.com>
2026-06-22 23:20:51 -0400

    repositioned

--- src/demos/userNeedDependency.ts
+++ src/demos/userNeedDependency.ts
@@ -4,10 +4,10 @@ export const userNeedDependencyDemo: DemoConfig = {
   viewBox: { width: 400, height: 300 },
   snapThreshold: 30,
   nodes: [
-    { id: "user", label: "User", x: 200, y: 26, draggable: false },
-    { id: "need", label: "Need", x: 200, y: 107, draggable: true, start: { x: 35, y: 107 } },
-    { id: "dependency-1", label: "Dependency", x: 130, y: 189, draggable: false },
-    { id: "dependency-2", label: "Dependency", x: 270, y: 189, draggable: false },
+    { id: "user", label: "User", x: 200, y: -6, draggable: false },
+    { id: "need", label: "Need", x: 200, y: 76, draggable: true, start: { x: 35, y: 76 } },
+    { id: "dependency-1", label: "Dependency", x: 130, y: 157, draggable: false },
+    { id: "dependency-2", label: "Dependency", x: 270, y: 157, draggable: false },
   ],
   connections: [
     { from: "user", to: "need" },
